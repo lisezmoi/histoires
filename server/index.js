f2b069610576958ee6fc2d3e1513ca1fa409cc61
Pierre Bertet <bonjour@pierrebertet.net>
2014-05-15 19:23:22 +0100

    Dynamic Cache-Control

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var compress = require('compression');
 var createTweets = require('./tweets');
 var fs = require('fs');
+var ONE_DAY = 86400000;
 
 var config = require('../config.json');
 config.application_only = true;
@@ -10,8 +11,6 @@ config.cacheDir = __dirname + '/../cache';
 var tweets = createTweets(config);
 
 var app = express();
-
-var ONE_DAY = 86400000;
 app.use(compress());
 app.use(express.static(__dirname + '/../client', { maxAge: ONE_DAY }));
 
@@ -24,7 +23,7 @@ app.get('/', (function() {
 
 app.get('/tweet/:id', function(req, res) {
   res.type('text/plain');
-  res.header('Cache-Control', 'public, max-age=86400');
+  res.header('Cache-Control', 'public, max-age=' + (ONE_DAY / 1000));
   tweets.embedCode(req.params.id).then(function(html) {
     res.end(html);
   }, function() {
